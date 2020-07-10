539c3e4d6d39cf378a1fdf418db7c775752f6ad0
John Slowik <john@jslowik.com>
2020-07-10 09:05:24 -0500

    Nav created

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'John Slowik',
+    description: 'My Site',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'About', link: '/about' },
+          { text: 'Resume', link: '/resume' },
+          { text: 'Posts', link: '/posts' }
+        ]
+      }
+  }
